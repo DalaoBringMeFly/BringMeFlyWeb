a781f57befbf0142edeb217f8a5d4621c87c2474
emilyblues <liaolei3@mail2.sysu.edu.cn>
2018-12-14 20:09:06 +0800

    add new page about

---
+++ src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Input search enter-button="Search" @on-search="handleSearch" style="width: 60%;margin:10px auto" placeholder="Enter something..." />
+    <Input v-model="Infor" type="textarea" :readonly="true" :rows="8" style="width: 60%;margin:10px auto" placeholder="Information..." />
+    <br />
+    <button name = "test" v-on:click='show = !show'>
+    Toggle
+    </button>
+  </div>
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped>
+
+</style>
